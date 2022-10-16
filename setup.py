05adc73e53d31bd94864faf62d1f738387908647
shiv-dhawan <shividhawan114@gmail.com>
2022-10-16 11:29:19 +0530

    Added required files and folders to the repo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'sensor',
+    version = '0.0.1',
+    author = 'shivanshu',
+    author_email='shividhawan114@gmail.com',
+    packages= find_packages()
+)

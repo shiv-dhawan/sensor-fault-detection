929c15abc449fed599ae55459399283e9e8408d2
shiv-dhawan <shividhawan114@gmail.com>
2022-10-16 14:05:49 +0530

    push all the files and folders for project

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import find_packages,setup
 
+def get_requirements()->'list[str]':
+    '''
+    This function will returns a list of requirements
+    '''
+    requirement_list:list[str] = []
+    return requirement_list
+
+
 setup(
     name = 'sensor',
     version = '0.0.1',
     author = 'shivanshu',
     author_email='shividhawan114@gmail.com',
     packages= find_packages(),
-    install_requires = []
+    install_requires = get_requirements()
 )
